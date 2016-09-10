ad013f8e663d1af35a32d731ce4fb32f1656c773
jhunexjun <jdream_catcher@yahoo.com>
2016-09-11 04:51:34 +0800

    Removed unnecessary code. I can now ask token from OAUTH2.0.

--- yourebusiness/routes/web.php
+++ yourebusiness/routes/web.php
@@ -15,10 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-/*Auth::routes();
-
-Route::get('/home', 'HomeController@index');*/
-
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
