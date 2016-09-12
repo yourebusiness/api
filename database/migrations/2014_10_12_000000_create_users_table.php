40254815194958daa769c91d66722564f6064311
jhunexjun <jdream_catcher@yahoo.com>
2016-09-13 03:59:55 +0800

    Reinitialized.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,22 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->smallInteger('id');
-            $table->string('name');
+            $table->integer('companyId');
+            $table->smallInteger('userId');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('fName');
+            $table->string('midName');
+            $table->string('lName');
+            $table->string('address');
+            $table->enum('gender', ['M', 'F']);
+            $table->dateTime('lastLogin');
+            $table->smallInteger('updatedBy');
+            $table->smallInteger('createdBy');
+            $table->boolean('active');
+            $table->boolean('activated');
+            $table->tinyInteger('role');
+            $table->boolean('trans');
             $table->rememberToken();
             $table->timestamps();
         });
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
-To be written.
+To begin development after the clone.
+1. $ npm install
+2. $ composer install
